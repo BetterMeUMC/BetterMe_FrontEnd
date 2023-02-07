64398a2cb73f964707d1c0f8fa14ad24a4bdda3a
Bella <88245113+shineju@users.noreply.github.com>
2023-02-07 19:53:24 +0900

    Revert "Revert "Merge branch 'main' into Bella""
    This reverts commit 6661ed12046475f77623b95636541db60e3559b1.

--- Better-me-iOS/AddGoodHabitViewController.swift
+++ Better-me-iOS/AddGoodHabitViewController.swift
@@ -112,7 +112,7 @@ class AddGoodHabitViewController: UIViewController {
     @IBAction func tapSearchButton(_ sender: UIButton) {
         guard let modalViewController = self.storyboard?.instantiateViewController(identifier: "SearchFriendViewController") as? SearchFriendViewController else { return }
         modalViewController.definesPresentationContext = true
-        modalViewController.modalPresentationStyle = .overCurrentContext
+        modalViewController.modalPresentationStyle = .overFullScreen
         modalViewController.inviteText = self.inviteTextField.text ?? ""
         navigationController?.present(modalViewController, animated: true, completion: nil)
         
