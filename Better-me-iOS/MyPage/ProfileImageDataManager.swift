caad382e0b91a7fc5e15004c5fa15183c167aa89
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-02-14 23:55:16 +0900

    Update ProfileImageDataManager.swift

--- Better-me-iOS/MyPage/ProfileImageDataManager.swift
+++ Better-me-iOS/MyPage/ProfileImageDataManager.swift
@@ -12,16 +12,16 @@ class ProfileImageDataManager {
     
     let token = UserDefaults.standard.string(forKey: "token")
     let userIdx = UserDefaults.standard.string(forKey: "userIdx")
-   
+    
     func patchProfilePhoto(imgData: Data) {
         guard let url = URL(string: "http://54.180.13.219:3000/app/users/upload/\(userIdx ?? "")") else {return}
-            
+        
         let header: HTTPHeaders = [
             "Content-Type" : "multipart/form-data", "Accept":"application/json", "x-access-token": token ?? ""]
-    
+        
         
         AF.upload(multipartFormData: { multipartFormData in
-                multipartFormData.append(imgData, withName: "profile", fileName: "photo.png", mimeType: "image/png")
+            multipartFormData.append(imgData, withName: "profile", fileName: "photo.png", mimeType: "image/png")
         }, to: url, method: .patch, headers: header)
         .validate()
         .responseDecodable(of: patchUserResponseStruct.self) { response in
